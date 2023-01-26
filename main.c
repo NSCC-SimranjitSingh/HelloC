f498b1554d186cd55855b88121265a92729e0881
Mike LeBlanc <mike_leblanc232@hotmail.com>
2023-01-26 09:19:32 -0400

    ML was here

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main() {
     printf("Hello, World, it's Josh!\n");
 
     printf("Hey, it's Lauren!\n");
+    printf("It's SHOWTIME! Sorry, watched Beetlejuice recently.\n");
 
     printf("Howdy folks! Ian here.\n");
     return 0;
