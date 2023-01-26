2a3468ad04fbf9e2ceec114f221552f01d9ef3db
eCampusFall2022 <simranjit.singh@nscc.ca>
2023-01-26 13:59:03 +0000

    conflict resolve

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 int main() {
     printf("Hello, World!\n");
     printf("Hello, World, it's Josh!\n");
-    printf("Hello, from Greg!\n");
 
     printf("Hey, it's Lauren!\n");
     printf("Hi Everyone!  It's Sophie \n");
@@ -14,5 +13,6 @@ int main() {
     printf("Hello, it's Brandon!\n");
 
     printf("chance was here\n");
+    printf("Hello, from Greg!\n");
     return 0;
 }
