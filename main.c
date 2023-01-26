9fe336aa00f9604da3cb1e792092be06f6a4c1bb
Greg Pomeroy <116170278+Vesby@users.noreply.github.com>
2023-01-26 09:45:38 -0400

    2nd Change made from Greg to Greg's branch

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main() {
     printf("Hello, World!\n");
     printf("Hello, World, it's Josh!\n");
-    printf("Hello, from Greg!\n");
+    printf("Hello, from Greg (In Greg's branch)!\n");
 
     printf("Hey, it's Lauren!\n");
     printf("Hi Everyone!  It's Sophie \n");
