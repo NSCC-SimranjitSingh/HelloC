1f13d6342b2059c0852fd074fd571e6a01dc1dc4
eCampusFall2022 <simranjit.singh@nscc.ca>
2023-01-26 13:12:57 +0000

    Remove all code

--- main.c
+++ main.c
@@ -3,12 +3,5 @@
 int main() {
     printf("Hello, World!\n");
 
-
-    printf("Hello, Simranjit!\n");
-    printf("Hi Sim and class! From Josh \n");
-
-    printf("Hello, Simranjit22222!\n");
-
-
     return 0;
 }
